65ed8b8d21344d47ddc82356d08a0f2eb8b61240
ElenaDRFS <elenafullstack@gmail.com>
2023-11-29 13:40:29 +0100

    finish3

--- scripts.js
+++ scripts.js
@@ -30,37 +30,87 @@ const militaryHelicopter = [
     'assets/helicoptero-militar-5.jpg'
 ];
 
+
+
 class Gallery {
     constructor(civilImages, militaryImages) {
+        this.civilImages = civilImages; //este no lleva el this porque se lo estamos pasando desde el constructor y no desde algo dentro del objeto, si fuera este último caso ambos lados llevarían this
+        this.militaryImages = militaryImages;
+        
+
     }
     
     getRandomCivil() {
+        
+        let randomIndex = Math.floor(Math.random() * this.civilImages.length) //generamos numero aleatorio hasta la longitud del array con todas las img
+        
+        let randomImage = this.civilImages[randomIndex]
+        return randomImage;
+        
     }
     
     getRandomMilitary() {
+         
+        let randomIndex = Math.floor(Math.random() * this.militaryImages.length) //generamos numero aleatorio hasta la longitud del array con todas las img
+    
+         let randomImage = this.militaryImages[randomIndex]
+         return randomImage;
+
     }
     
     getAll() {
+        return [...this.civilImages, ...this.militaryImages];
+        
+     
     }
 }
 
+
 class Painter {
     constructor() {
+        this.gallery = this.createGallery();
     }
 
     createGallery() {
+        let section = document.createElement('section');
+        section.setAttribute('id','galleryid')
+        let body = document.getElementsByTagName('body')[0];
+        body.appendChild(section)
+        return document.getElementById('galleryid');
+        
+        
     }
 
     createImageTag(imageUrl) {
+
+        let picture = document.createElement('picture');
+        let img = document.createElement('img');
+        img.setAttribute('src', imageUrl);
+        picture.appendChild(img);
+        return picture;
     }
 
+    // añade a gallery lo que retorna createimagetag, así que llamamos a esa función 
     paintSingleImage(imageUrl) {
+        let imageTag = this.createImageTag(imageUrl);
+        this.gallery.appendChild(imageTag);
     }
 
+    //añade a gallery uno a uno los resultados de createimagetag, diferencia, hacer un bucle, lo recoreremos, añadimos una etiqueta por cada url encontrada y se la ponemos como appedn child a gallery, importate, dentro del bucle!!!
     paintMultipleImages(arrayOfImages) {
+        for(let elementUrl of arrayOfImages){
+            let imageTag = this.createImageTag(elementUrl);
+        this.gallery.appendChild(imageTag)
+
+        }
+       ;
+       
     }
 }
 
 const aircrafts = new Gallery(civilAircrafts, militaryAircrafts);
 const helicopters = new Gallery(civilHelicopters, militaryHelicopter);
 const painter = new Painter();
+
+
+
